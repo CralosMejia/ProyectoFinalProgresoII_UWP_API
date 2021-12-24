664915959cd6251174f54064ec67adc57f023368
calebdamian <naranjocaleb@gmail.com>
2021-12-24 13:54:16 -0500

    Se agrega búsqueda a la página Main de Pendientes y se cambia algunos diseños en cuanto a botones.

--- AgendaPlusUWP/Views/MainTasks.xaml.cs
+++ AgendaPlusUWP/Views/MainTasks.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using AgendaPlusUWP.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,9 +26,100 @@ namespace AgendaPlusUWP.Views
     /// </summary>
     public sealed partial class MainTasks : Page
     {
+
+
+        private static List<Pendientes> resultadoAPI;
+
         public MainTasks()
         {
+            llenarAsync();
             this.InitializeComponent();
         }
-    }
+
+
+        public async void llenarAsync(int userID = 1)
+        {
+       
+            var httpHandler = new HttpClientHandler();
+            var request = new HttpRequestMessage();
+
+            //definir todos los atributos del request
+
+            request.RequestUri = new Uri("https://localhost:44386/api/pendientes");
+            request.Method = HttpMethod.Get;
+            request.Headers.Add("Accept", "application/json");
+
+            //asignar cliente
+            var client = new HttpClient(httpHandler);
+
+            //usar palabra claver await con funciones async 
+
+            HttpResponseMessage response = await client.SendAsync(request);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            //se necesita instalar por NuGet JSON
+            //se deserializa el contenido para formatear de acuerdo a la interfaz
+            var resultado = JsonConvert.DeserializeObject<List<Pendientes>>(content);
+
+           // resultadoAPI = resultado.FirstOrDefault(x => x.UsuarioID == userID).Pendientes.ToList();
+
+            
+            ListaPendientes.ItemsSource = resultado;
+
+
+            resultadoAPI = resultado;
+
+            //ListaPendientes.ItemsSource = resultadoAPI;
+
+        }
+
+
+        private async void DisplayDeleteFileDialog()
+        {
+            ContentDialog deleteFileDialog = new ContentDialog
+            {
+                Title = "Delete task permanently?",
+                Content = "If you delete this fitask, you won't be able to recover it. Do you want to delete it?",
+                PrimaryButtonText = "Delete",
+                CloseButtonText = "Cancel"
+            };
+
+            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+
+            // Delete the file if the user clicked the primary button.
+            /// Otherwise, do nothing.
+            if (result == ContentDialogResult.Primary)
+            {
+                // Delete the file.
+            }
+            else
+            {
+                // The user clicked the CLoseButton, pressed ESC, Gamepad B, or the system back button.
+                // Do nothing.
+            }
+        }
+
+        private void buscar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string palabra = buscar.Text.ToUpper();
+
+         
+                if (palabra.Equals(""))
+                {
+                    llenarAsync();
+                }
+                else
+                {
+
+
+                    List<Pendientes> resultado = resultadoAPI.Where(x => x.Titulo.ToUpper().Contains(palabra)).ToList();
+
+                    ListaPendientes.ItemsSource = resultado;
+
+                }
+            }
+            
+        }
+    
 }
