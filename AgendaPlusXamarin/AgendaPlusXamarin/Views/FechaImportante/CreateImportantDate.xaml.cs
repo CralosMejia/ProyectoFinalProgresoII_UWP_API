60c458bddcd1297b5192aa1f28b38248866d6cb2
Carlos Mejia <camh670@gmail.com>
2022-01-26 15:57:56 -0500

    Agregue el crear fecha importante, tanto sus vistas como sus funcionalidades.

---
+++ AgendaPlusXamarin/AgendaPlusXamarin/Views/FechaImportante/CreateImportantDate.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AgendaPlusXamarin.Views.FechaImportante
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateImportantDate : ContentPage
+    {
+        private FechaImportante fecha;
+        private int userID;
+
+        public CreateImportantDate(int userIDParam)
+        {
+            userID = userIDParam;
+            InitializeComponent();
+            datePi.MinimumDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+        }
+
+        private async void closedButton(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private async void createDate(object sender, EventArgs e)
+        {
+
+            if (dateDescription.Text != null && dateTitle.Text != null)
+            {
+                fecha = new FechaImportante { Titulo = dateTitle.Text, Descripcion = dateDescription.Text, UsuarioID = userID, FechaLimite = datePi.Date };
+
+                FechaImportanteController.postFecha(fecha);
+
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                await DisplayAlert("Alert", "All fields must be completed.", "OK");
+            }
+        }
+    }
+}
